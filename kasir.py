8d425a0036303c902416befad5ad54870877d996
YulindaFitri <160814495+YulindaFitri@users.noreply.github.com>
2024-02-29 11:26:51 +0700

    Add files via upload

---
+++ kasir.py
@@ -0,0 +1,16 @@
+Rokok = 20000
+Minyak = 25000
+Gula = 11000
+Saldo = 100000
+diskon = 10000
+
+total_belanja = Rokok+Minyak+Gula
+total_diskon = total_belanja-diskon
+kembalian = Saldo-total_diskon
+
+
+print("total belanja ucup:", Rokok+Minyak+Gula)
+print("total diskon ucup", diskon)
+print("saldo ucup:", Saldo)
+print("kembalian ucup:", kembalian)
+
